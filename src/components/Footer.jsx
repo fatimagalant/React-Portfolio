ab781c3fefee4f7c34ac0e486d0471fcac4bd18e
fatimagalant <fatimagalant03@gmail.com>
2024-03-05 16:07:04 +0200

    bg-image, skill icon styles, about text + image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,21 @@
 export default function Footer() {
     return(
-        <section id="footer">I am a footer</section>
+        <section id="footer">
+            <div className="text-white p-5">
+                <div className="grid-rows-1">
+                    <div className="grid-cols-3">
+                        <div>
+                            <h6>About</h6>
+                            </div>
+                        <div>
+                            <h6>Quick Links</h6>
+                            </div>
+                        <div>
+                            <h6>Contact</h6>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
