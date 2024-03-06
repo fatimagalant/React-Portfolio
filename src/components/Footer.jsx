5ec4bb70dc287aab13f1b3d6741b2b30a77f620e
fatimagalant <fatimagalant03@gmail.com>
2024-03-06 14:41:44 +0200

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,42 @@
 export default function Footer() {
     return(
         <section id="footer">
-            <div className="text-white p-5">
+            <div className="text-white border-2">
                 <div className="grid-rows-1">
-                    <div className="grid-cols-3">
+                    <div className="grid grid-cols-3 justify-center p-5">
                         <div>
-                            <h6>About</h6>
-                            </div>
+                            <a className="heading font-semibold title-font font-medium text-white mb-2 md:mb-0">
+                                <a href="#home" className="ml-3 text-center text-white">FG</a>
+                            </a>     
+                        </div>
                         <div>
-                            <h6>Quick Links</h6>
-                            </div>
+                            <h6 className="heading text-xl text-center">Quick Links</h6>
+                            <ul className="text-center">
+                                <li><a href="#about" className="mr-5 font-medium text-white hover:text-gray">About</a></li>
+                                <li>  <a href="#skills" className="mr-5 font-medium text-white hover:text-gray">Skills</a></li>
+                                <li> <a href="#projects" className="mr-5 font-medium text-white hover:text-gray">Projects</a></li>
+                                <li>  <a href="#resume" className="mr-5 font-medium text-white hover:text-gray">Resume</a></li>
+                                <li><a href="#testimonials" className="mr-5 font-medium text-white hover:text-gray">Testimonials</a></li>
+                            </ul>
+                        </div>
                         <div>
-                            <h6>Contact</h6>
+                            <h6 className="heading text-xl text-center"><a href="#contact">Contact</a></h6>
+                            <ul className="text-center">
+                                <li><a href="tel:0662091181">Give me a call</a></li>
+                                <li><a href="mailto:fatimagalant03@gmail.com">Email Me</a></li>
+                            </ul>
                             </div>
                     </div>
+                </div>
+                <hr/>
+                <div className="grid-rows-1 justify-center">
+                <div className="grid grid-cols-2 p-2">
+                    <p className="text-center">Copyright © 2022 All Rights Reserved</p>
+                    <div className="socials text-3xl text-center">
+                   <a href="https://github.com/fatimagalant" target="_blank"><i class="fa-brands fa-github mb-16" className="mb-0"></i></a>
+                    <a href="https://www.linkedin.com/in/fatimagalant/" target="_blank"><i class="fa-brands fa-linkedin" className="mb-0"></i></a>
+                    </div>
+                </div>
                 </div>
             </div>
         </section>
